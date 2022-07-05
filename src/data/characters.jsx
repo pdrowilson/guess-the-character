a553e9dacdc05a3a81598fa65bb0494623b80571
pdrowilson <pdro.wilson@gmail.com>
2022-07-05 16:14:58 -0300

    adding data

---
+++ src/data/characters.jsx
@@ -0,0 +1,10 @@
+const animeCharacters = {
+  naruto: ['Naruto', 'Kakashi', 'Sasuke', 'Sakura', 'Jiraya'],
+  onepiece: ['Luffy', 'Zoro', 'Ace', 'Sanji', 'Nami'],
+  jojo: ['Joseph Joestar', 'Dio', 'Jolyne Kujo', 'Jotaro', 'Josuke'],
+  hunterxhunter: ['Leorio', 'Gon', 'Hisoka', 'Killua', 'Kurapika'],
+  bleach: ['Ichigo', 'Byakuya', 'Yoruichi', 'Kisuke Urahara', 'Kenpachi Zaraki'],
+  dragonball: ['Goku', 'Gohan', 'Cell', 'Freeza', 'Vegeta', 'Bulma'],
+};
+
+export default animeCharacters;
